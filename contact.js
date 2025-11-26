224eb38e25b97ad0ffd1a013b1cdfa4548f43c70
milabtv <vboteva.mila@gmail.com>
2025-11-26 16:25:18 +0200

    contact

--- contact.js
+++ contact.js
@@ -1,12 +1,12 @@
 
-(function() {
+(function () {
   emailjs.init("o6xZVMPNkI1Ch3geb"); // 
 })();
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector(".contact-form");
 
-  form.addEventListener("submit", function(e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     emailjs.sendForm("service_designsverse", "template_vfw9947", this)
