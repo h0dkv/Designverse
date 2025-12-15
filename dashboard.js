058f5ffb3cf9d26abc0cc516b71fdccf87bfcf1d
h0dkv <hristian.26v@oupvolov.com>
2025-12-15 14:48:08 +0200

    dashboard

---
+++ dashboard.js
@@ -0,0 +1,41 @@
+import { auth } from "./firebase-init.js";
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginLink = document.getElementById("login-link");
+  const dashboard = document.getElementById("dashboard");
+  const dashboardBtn = document.getElementById("dashboard-btn");
+  const dashboardMenu = document.getElementById("dashboard-menu");
+  const logoutBtn = document.getElementById("logout-btn");
+  const greeting = document.getElementById("user-greeting");
+
+
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      loginLink.style.display = "none";
+      dashboard.style.display = "block";
+      greeting.textContent = `Здравей, ${user.email}!`;
+    } else {
+      loginLink.style.display = "inline-block";
+      dashboard.style.display = "none";
+    }
+  });
+
+  dashboardBtn.addEventListener("click", () => {
+    dashboardMenu.classList.toggle("show");
+  });
+
+  logoutBtn.addEventListener("click", async () => {
+    await signOut(auth);
+    window.location.href = "index.html";
+  });
+
+  document.addEventListener("click", e => {
+    if (!dashboard.contains(e.target)) {
+      dashboardMenu.classList.remove("show");
+    }
+  });
+});
